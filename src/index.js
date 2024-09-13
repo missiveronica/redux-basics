9d0a1547b534e000ae2bd255fe82bdae3f7ac3a0
missiveronica <veronica_glu@yahoo.com>
2024-09-13 10:25:07 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,55 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+//1. STORE - all 
+
+
+// 2. Acion
+
+//A
+const addToCart = () => {
+  return {
+    type: "ADD_TO_CART"
+  }
+}
+//B 
+const removeItem = () => {
+  return {
+    type: "REMOVE_ITEM"
+  }
+}
+//C 
+const changeNumber = () => {
+  return {
+    type: "CHANGE_NUMBER"
+  }
+}
+
+//3. REDUCER 
+
+const cart = (state = 0, action) => {
+  switch(action.type) {
+    case "ADD_TO_CART":
+      return state +1;
+
+      case "REMOVE_ITEM":
+        return state -1;
+        
+        default:
+          return state;
+  }
+}
+let store = createStore(cart); 
+
+// 4. Dispatch
+store.dispatch (addToCart());
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
